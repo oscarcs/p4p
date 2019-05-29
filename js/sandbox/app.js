6f49200143efdcc363774ed6c9a634bb8e76af23
Oscar Sims <nxtrafalgar@yahoo.co.nz>
2019-05-29 21:04:40 +1200

    Add Phaser.

---
+++ js/sandbox/app.js
@@ -0,0 +1,22 @@
+var game = new Phaser.Game({
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+});
+
+function preload () {
+
+}
+
+function create () {
+
+}
+
+function update () {
+    
+}
