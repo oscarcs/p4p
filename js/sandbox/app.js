32c5ebb82be21c1609851e553854a182b564d5a9
12452williamning@gmail.com <12452williamning@gmail.com>
2019-06-16 20:22:50 +1200

    initial sandbox implementation

--- js/sandbox/app.js
+++ js/sandbox/app.js
@@ -1,79 +1,111 @@
-var game = new Phaser.Game({
+class mainScene extends Phaser.Scene
+{   
+    map; //the map that the sprites are placed upon
+
+    constructor ()
+    {
+        super("Game_Scene");
+    }
+
+    preload () {
+
+        this.load.spritesheet('tiles', '../assets/tilesheet.png', {
+            frameWidth: 16,
+            frameHeight: 16
+        });
+    }    
+
+    create () {
+        this.map = this.make.tilemap({
+            width: 100, 
+            height: 100,
+            tileWidth: 16,
+            tileHeight: 16,
+        });
+
+        let tiles = this.map.addTilesetImage('tiles', null, 16, 16);
+
+        let base = this.map.createBlankDynamicLayer('base', tiles);
+        this.map.fill(2, 0, 0, this.map.width, this.map.height, 'base');        
+        
+        this.scene.launch("Block_Menu");   
+
+    }
+
+    update () {
+        //TODO use this to render all drawn blocks.
+                       
+    }
+
+    createSnowDrift(x, y) {
+        this.map.putTileAt(12, x, y, 'base');
+    }
+
+    createTree(x, y) {
+        this.map.putTileAt(0, x, y, 'objects');
+        this.map.putTileAt(10, x, y + 1, 'objects');
+        this.map.putTileAt(20, x, y + 2, 'objects');
+    }
+
+    createHouse(x, y) {
+        this.map.putTileAt(3, x, y, 'objects');
+        this.map.putTileAt(4, x + 1, y, 'objects');
+        this.map.putTileAt(5, x + 2, y, 'objects');
+        this.map.putTileAt(13, x, y + 1, 'objects');
+        this.map.putTileAt(14, x + 1, y + 1, 'objects');
+        this.map.putTileAt(15, x + 2, y + 1, 'objects');
+    }
+
+    createDeer(x, y) {
+        this.map.putTileAt(1, x, y, 'objects');
+    }
+
+}
+
+
+class tileSelection extends Phaser.Scene{
+    selected;
+
+    constructor ()
+    {
+        super("Block_Menu");
+    }
+
+    create(){
+        let baseScene = this.scene.get("Game_Scene");
+
+        var treeButton = this.add.text(10, 10, 'Tree')
+        .setInteractive()
+        .on('pointerdown', () => { 
+            console.log("Tree Sprite");
+            this.selected="tree"; 
+            baseScene.createTree(10,10);                  
+           
+        });
+
+        var mooseButton = this.add.text(60, 10, 'Moose')
+        .setInteractive()
+        .on('pointerdown', () => { 
+            console.log("Moose Sprite");
+            this.selected = "moose";
+            baseScene.createDeer(10,10);  
+        });        
+    }
+
+    update(){
+
+    }
+
+}
+
+var config = {
     type: Phaser.AUTO,
     width: 320,
     height: 240,
     parent: 'sandbox',
     pixelArt: true,
     zoom: 2,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
-});
-
-function preload () {
-
-    this.load.spritesheet('tiles', '../assets/tilesheet.png', {
-        frameWidth: 16,
-        frameHeight: 16
-    });
+    scene: [mainScene, tileSelection]
 }
 
-function create () {
-    let map = this.make.tilemap({
-        width: 100, 
-        height: 100,
-        tileWidth: 16,
-        tileHeight: 16,
-    });
-
-    let tiles = map.addTilesetImage('tiles', null, 16, 16);
-
-    let base = map.createBlankDynamicLayer('base', tiles);
-    map.fill(2, 0, 0, map.width, map.height, 'base');
-
-    for (let i = 0; i < 10; i++) {
-        createSnowDrift(map, Math.floor(Math.random() * 20), Math.floor(Math.random() * 16));
-    }
-
-    let objs = map.createBlankDynamicLayer('objects', tiles);
-
-    
-    for (let i = 0; i < 3; i++) {
-        createDeer(map, Math.floor(Math.random() * 20), Math.floor(Math.random() * 16));
-    }
-    
-    for (let i = 0; i < 15; i++) {
-        createTree(map, Math.floor(Math.random() * 20), Math.floor(Math.random() * 16));
-    }
-
-    createHouse(map, 8, 5);
-}
-
-function update () {
-
-}
-
-function createSnowDrift(map, x, y) {
-    map.putTileAt(12, x, y, 'base');
-}
-
-function createTree(map, x, y) {
-    map.putTileAt(0, x, y, 'objects');
-    map.putTileAt(10, x, y + 1, 'objects');
-    map.putTileAt(20, x, y + 2, 'objects');
-}
-
-function createHouse(map, x, y) {
-    map.putTileAt(3, x, y, 'objects');
-    map.putTileAt(4, x + 1, y, 'objects');
-    map.putTileAt(5, x + 2, y, 'objects');
-    map.putTileAt(13, x, y + 1, 'objects');
-    map.putTileAt(14, x + 1, y + 1, 'objects');
-    map.putTileAt(15, x + 2, y + 1, 'objects');
-}
-
-function createDeer(map, x, y) {
-    map.putTileAt(1, x, y, 'objects');
-}
+let game = new Phaser.Game(config);
